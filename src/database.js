2f86f9ca9722bcb10be2c642193210605ad4055f
VernaMai <vmai1@my.bcit.ca>
2023-03-31 16:44:09 -0700

    1

--- src/database.js
+++ src/database.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
 });
 
 
